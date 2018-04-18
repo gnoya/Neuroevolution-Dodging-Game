3486cf7b1d19a264c604f3021b471627e87a9438
Gabriel <gabrielnoya95@gmail.com>
2018-04-17 23:54:42 -0400

    One block and one player with keyboard

--- block.js
+++ block.js
@@ -1,5 +1,24 @@
 class Block {
-  constructor() {
+  constructor(x, y, width, height, xVelocity, yVelocity) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(xVelocity, yVelocity);
+    this.width = width;
+    this.height = height;
+  }
 
+  update() {
+    this.position.add(this.velocity);
+  }
+
+  offScreen(){
+    if(this.position.x + this.width / 2 < 0 || this.position.x - this.width / 2 > width || this.position.y - this.height / 2 > height || this.position.y + this.height / 2 < 0){
+      return true;
+    }
+    return false;
+  }
+
+  show() {
+    fill(0);
+    rect(this.position.x, this.position.y, this.width, this.height);
   }
 }
--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-function setup() {
+const blockSize = 50;
+const playerSize = 25;
+const blockSpeed = 4;
+const targetSpeed = 4;
 
+let blocks = new Array();
+let players = new Array();
+
+function setup() {
+  createCanvas(800, 800);
+  rectMode(CENTER);
+  blocks.push(new Block(400, 100, blockSize, blockSize, 0, 5));
+  players.push(new Player(400, 700, playerSize, playerSize));
 }
 
 function draw() {
+  background(220);
+  for (let block of blocks) {
+    block.update();
+    block.show();
+    if (block.offScreen()) {
+      blocks.splice(blocks.indexOf(block), 1); 
+    }
+  }
 
+  for(let player of players){
+    if (player.crashed(blocks)) {
+      players.splice(players.indexOf(player), 1);
+    }
+    player.show();
+  }
+}
+
+function keyPressed() {
+  if (keyCode  == LEFT_ARROW) {
+    players[0].left();
+  }
+  else if (keyCode  == RIGHT_ARROW){
+    players[0].right();
+  }
 }
